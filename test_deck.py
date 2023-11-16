90410d4c88ffc9040b8e048dc8b3a375cc599ff3
bedo.kh <bedo.kharboutli@hotmail.com>
2023-11-16 21:24:41 +0100

    Added some tests

--- test_deck.py
+++ test_deck.py
@@ -5,9 +5,30 @@ from deck import Deck, Card
 
 def test_card():
     # Test här
-    pass
+    card = Card(4, "D")
+    assert card.__str__ == "4D"
 
 
 def test_deck():
     # Test här
-    pass
+    deck = Deck()
+    assert len(deck) == 52
+
+
+def shuffle_deck():
+    deck = Deck()
+    shuffle_deck = deck.shuffle()
+    assert not deck == shuffle_deck
+
+
+def test_take():
+    deck = Deck()
+    deck.take()
+    assert len(deck) == 51
+
+
+def test_put():
+    card = Card(7, "D")
+    deck = Deck()
+    deck.put(card)
+    assert len(deck) == 53
