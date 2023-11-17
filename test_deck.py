c7efd24f2b1b02c9ea5af71843703669d2ee0278
THI LY TA <92649607+thienly1@users.noreply.github.com>
2023-11-17 14:13:21 +0100

    Add files via upload

--- test_deck.py
+++ test_deck.py
@@ -8,47 +8,60 @@ def test_card_init():
     assert(str(card) == "4F")
 
 def test_card_eq():
-#Det testar om metoden __eq__ (jämförelse av likhet) fungerar korrekt för två kort med samma värden.
     card1 = Card(4,"FirstCard")
     card2 = Card(4,"SecondCard")
     assert card1.__eq__(card2) == True
 
 def test_card_lt():
-    #Det testar om metoden __lt__ (mindre än) fungerar korrekt för två kort med olika värden.
     card1 = Card(4, "FirstCard")
     card2 = Card(2, "SecondCard")
     assert card2.__lt__(card1) == True
 
 def test_card_gt():
-    #Det testar om metoden __gt__ (större än) fungerar korrekt för två kort med olika värden.
     card1 = Card(4, "FirstCard")
     card2 = Card(2, "SecondCard")
     assert card1.__gt__(card2) == True
 
 def test_deck_len():
-    #Det testar om metoden __len__ för Deck returnerar korrekt antal kort i kortleken.
     deck = Deck()
     assert deck.__len__() == 52
 
 def test_deck_insert():
-    #Det testar när man lägger till ett kort på rätt plats i kortleken, det är viktigt föratt säkerställa att de hamnar på rätt plats.
     card_list = [Card(1, "FirstCard"),Card(2,"SecondCard"), Card(2, "FirstCard"), Card(3, "FirstCard"), Card(4,"SecondCard"), Card(4,"ThirdCard"), Card(5, "FouthCard")]
     card = Card(3, "AnotherCard")
     card_list_after_insert = [Card(1, "FirstCard"),Card(2,"SecondCard"), Card(2, "FirstCard"),Card(3, "AnotherCard"), Card(3, "FirstCard"), Card(4,"SecondCard"), Card(4,"ThirdCard"), Card(5, "FouthCard")]
     assert Deck.insert(card_list,card) == card_list_after_insert
 
 def test_deck_take():
-    #Det testar om metoden take() för Deck minskar antalet kort i kortleken from 52 till 51.
     deck = Deck()
     deck.take()
     assert deck.__len__() == 51
 
 def test_deck_put():
-     #Det testar om metoden put() för Deck höjer antalet kort i kortleken from 52 till 53 när man lägger till ett kort.
     deck = Deck()
     deck.put(Card(8, "NewCard"))
     assert deck.__len__() == 53
     assert Card(8,"NewCard") in deck.cards
+def test_deck_sort():
+    deck = Deck()
+    deck.sort()
+    # ['Hearts', 'Diamonds', 'Spades', 'Clubs']
+    actual = [Card(1,"Hearts"), Card(1,'Diamonds'),Card(1,'Spades'), Card(1,'Clubs'),
+              Card(2,"Hearts"), Card(2,'Diamonds'),Card(2,'Spades'), Card(2,'Clubs'),
+              Card(3,"Hearts"), Card(3,'Diamonds'),Card(3,'Spades'), Card(3,'Clubs'),
+              Card(4,"Hearts"), Card(4,'Diamonds'),Card(4,'Spades'), Card(4,'Clubs'),
+              Card(5,"Hearts"), Card(5,'Diamonds'),Card(5,'Spades'), Card(5,'Clubs'),
+              Card(6,"Hearts"), Card(6,'Diamonds'),Card(6,'Spades'), Card(6,'Clubs'),
+              Card(7,"Hearts"), Card(7,'Diamonds'),Card(7,'Spades'), Card(7,'Clubs'),
+              Card(8,"Hearts"), Card(8,'Diamonds'),Card(8,'Spades'), Card(8,'Clubs'),
+              Card(9,"Hearts"), Card(9,'Diamonds'),Card(9,'Spades'), Card(9,'Clubs'),
+              Card(10,"Hearts"), Card(10,'Diamonds'),Card(10,'Spades'), Card(10,'Clubs'),
+              Card(11,"Hearts"), Card(11,'Diamonds'),Card(11,'Spades'), Card(11,'Clubs'),
+              Card(12,"Hearts"), Card(12,'Diamonds'),Card(12,'Spades'), Card(12,'Clubs'),
+              Card(13,"Hearts"), Card(13,'Diamonds'),Card(13,'Spades'), Card(13,'Clubs')]
+
+    assert deck.cards == actual
+
 
 
 
